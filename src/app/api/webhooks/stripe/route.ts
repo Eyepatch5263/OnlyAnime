d120a5948e545fb362c0e6491a3d3b8fe9aa054e
Eyepatch5263 <pratyushpragyey@gmail.com>
2024-07-03 19:08:25 +0530

    first commit

--- src/app/api/webhooks/stripe/route.ts
+++ src/app/api/webhooks/stripe/route.ts
@@ -173,6 +173,14 @@ export async function POST(req: Request) {
                 }
                 break
             }
+            case "checkout.session.expired":{
+                const session=await stripe.checkout.sessions.retrieve((data.object as Stripe.Checkout.Session).id)
+                await prisma.order.delete({
+                    where:{id:session.metadata?.orderId}
+                })
+                break
+            }
+
             default:
                 console.log(`Unhandled event type: ${eventType}`)
                 break
